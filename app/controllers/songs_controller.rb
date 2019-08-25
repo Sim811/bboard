480ef964168ddf9ea7c2d7eabe8234008a754b41
Sim <skmatagi@hotmail.com>
2019-08-25 16:48:05 -0600

    newest commit

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,12 +2,33 @@ class SongsController < ApplicationController
   before_action :set_artist
   before_action :set_song, only: [:show, :edit, :update, :destroy]
   
+  def index
+    @song = Songs.all
+  end
+  
   def new
-    @song = @artist.songs.new
+    @song = @artist.songs.new()
   end
 
   def show
     @song = Song.find(params[:id])
+  end
+
+  def create
+    @song = Song.new()
+    if @song.save
+      redirect_to artist_songs_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @song.update(song_params)
+      redirect_to artist_songs_path(@song)
+    else
+      render :edit
+    end
 
   end
 
